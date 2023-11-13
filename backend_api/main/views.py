26be6aae7001e1b4f83ca5a6c50ddb2c72b81d37
CarolineMwenda <carolinemwenda162@gmail.com>
2023-11-14 00:47:32 +0300

    first commit

---
+++ backend_api/main/views.py
@@ -0,0 +1,66 @@
+from rest_framework import generics,permissions,pagination,viewsets
+from . import serializers
+from . import models
+
+
+# Create your views here.
+class VendorList(generics.ListAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorSerializer
+    
+    
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorDetailSerializer
+    
+class ProductList(generics.ListAPIView):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductListSerializer
+    pagination_class = pagination.LimitOffsetPagination
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductDetailSerializer
+
+#customer
+class CustomerList(generics.ListCreateAPIView):
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomerSerializer
+    
+    
+class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomerDetailSerializer
+
+#Order
+class OrderList(generics.ListCreateAPIView):
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
+
+class OrderDetail(generics.ListAPIView):
+   # queryset = models.OrderItems.objects.all()
+    serializer_class = serializers.OrderDetailSerializer
+    
+    def get_queryset(self):
+        Order_id = self.kwargs['pk']
+        order = models.Order.objects.get(id=Order_id)
+        order_items=models.OrderItems.objects.filter(order=order)
+        return order_items
+    
+class CustomerAddressViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.CustomerAddressSerializer
+    queryset = models.CustomerAddress.objects.all()
+
+class ProductRatingViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ProductRatingSerializer
+    queryset = models.ProductRating.objects.all()
+
+#Category List API
+class CategoryList(generics.ListCreateAPIView):
+    queryset = models.ProductCategory.objects.all()
+    serializer_class = serializers.CategorySerializer
+    
+    
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.ProductCategory.objects.all()
+    serializer_class = serializers.CategoryDetailSerializer
